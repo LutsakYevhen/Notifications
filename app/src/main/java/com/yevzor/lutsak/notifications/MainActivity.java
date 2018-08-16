587af655815f1d3067ba92fcaa62f45e6767efaf
Lutsak Yevhen <yev.lutsak@gmail.com>
2018-08-16 20:01:44 +0300

    On button clicked notification appear

--- app/src/main/java/com/yevzor/lutsak/notifications/MainActivity.java
+++ app/src/main/java/com/yevzor/lutsak/notifications/MainActivity.java
@@ -1,14 +1,49 @@
 package com.yevzor.lutsak.notifications;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private Button mNotificationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "--> onCreate");
+
+        mNotificationButton = findViewById(R.id.notification_button);
+        mNotificationButton.setOnClickListener(this);
+
+        Log.d(TAG, "<-- onCreate");
+
     }
 
+    @Override
+    public void onClick(View view) {
+        Log.d(TAG, "onClick");
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setContentTitle("Notification")
+                        .setContentText("It's example of notification text");
+
+        Notification notification = builder.build();
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        if (notificationManager != null) {
+            notificationManager.notify(1, notification);
+        }
+    }
 }
